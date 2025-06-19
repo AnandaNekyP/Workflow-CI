b5080dd03c54b43169b9517e4929c96255d83661
AnandaNekyP <mr.anandanekyputra@gmail.com>
2025-06-19 13:23:48 +0700

    Remove commented print statement for cleaner code

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -39,4 +39,4 @@ with mlflow.start_run():
     df = load_data(INPUT_PATH)
     x_train, y_train, x_test, y_test = split_data(df)
     model = train_model(x_train, y_train)
-    print(mlflow.)
+    # print(mlflow.)
