4743d0cbda2e99ce22b8ae5aa1d32be20120e94a
AnandaNekyP <mr.anandanekyputra@gmail.com>
2025-06-19 11:27:30 +0700

    Refactor modelling.py to remove unused visualizer functions and imports; clean up requirements.txt by removing matplotlib and yellowbrick

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,22 +1,12 @@
 import pandas as pd
 import numpy as np
 import mlflow
-import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import GradientBoostingClassifier
-from yellowbrick.target import ClassBalance
-from yellowbrick.classifier import (
-    ROCAUC,
-    PrecisionRecallCurve,
-    ClassificationReport,
-    ClassPredictionError,
-    DiscriminationThreshold,
-    ConfusionMatrix,
-)
-
 
 import dagshub
-dagshub.init(repo_owner='AnandaNekyP', repo_name='Membangun_model', mlflow=True)
+
+dagshub.init(repo_owner="AnandaNekyP", repo_name="Membangun_model", mlflow=True)
 mlflow.set_tracking_uri("https://dagshub.com/AnandaNekyP/Membangun_model.mlflow")
 
 # mlflow.set_tracking_uri("http://localhost:5000")
@@ -31,10 +21,6 @@ def load_data(path):
 
 
 def split_data(df):
-    cb = ClassBalance(labels=["Not Hired", "Hired"])
-    cb.fit(df["HiringDecision"])
-    cb.show(outpath="viz/Class Balance.png")
-
     train_df, test_df = train_test_split(df, test_size=0.2, random_state=42)
 
     train_dataset = mlflow.data.from_pandas(train_df, name="train")
@@ -53,29 +39,7 @@ def train_model(x_train, y_train):
     return model
 
 
-def evaluate_model(model, x_train, y_train, x_test, y_test):
-    classes = ["Not Hired", "Hired"]
-
-    visualizers = {
-        "Classification Report": ClassificationReport(
-            model, classes=classes, support=True
-        ),
-        "Confusion Matrix": ConfusionMatrix(model, classes=classes),
-        "ROC AUC": ROCAUC(model, classes=classes),
-        "Precision Recall Curve": PrecisionRecallCurve(model, classes=classes),
-        "Class Prediction Error": ClassPredictionError(model, classes=classes),
-        # "Discrimination Threshold": DiscriminationThreshold(model, classes=classes),
-    }
-    for name, viz in visualizers.items():
-        viz.fit(x_train, y_train)
-        viz.score(x_test, y_test)
-        viz.show(outpath=f"viz/{name}.png", clear_figure=True)
-        plt.close('all')
-
-
-
 with mlflow.start_run():
     df = load_data(INPUT_PATH)
     x_train, y_train, x_test, y_test = split_data(df)
     model = train_model(x_train, y_train)
-    evaluate_model(model, x_train, y_train, x_test, y_test)
